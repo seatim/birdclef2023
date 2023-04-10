5ad8c134b27bbf5c969b31980fc0a7d782479354
tim@brandywine <tim@brandywine>
2023-04-10 13:38:50 -0700

    Add --frame-duration option to make_images_from_audio.py

--- make_images_from_audio.py
+++ make_images_from_audio.py
@@ -9,7 +9,8 @@ import numpy as np
 
 from PIL import Image
 
-from adak.transform import images_from_audio, DEFAULT_SAMPLE_RATE
+from adak.transform import (images_from_audio, DEFAULT_SAMPLE_RATE,
+                            DEFAULT_FRAME_DURATION)
 
 DEFAULT_AUDIO_DIR = 'data/train_audio'
 DEFAULT_IMAGES_DIR = 'data/train_images'
@@ -20,13 +21,15 @@ def save_image(img, path):
     Image.fromarray(np.uint8(255*img), 'L').save(path, 'PNG')
 
 
-def make_images_for_class(label, images_dir, audio_dir, max_examples):
+def make_images_for_class(label, images_dir, audio_dir, max_examples,
+                          frame_duration=DEFAULT_FRAME_DURATION):
     cls_dir = join(audio_dir, label)
     img_count = 0
 
     for name in os.listdir(cls_dir):
         images = images_from_audio(
-            join(cls_dir, name), assert_sr=DEFAULT_SAMPLE_RATE)
+            join(cls_dir, name), assert_sr=DEFAULT_SAMPLE_RATE,
+            frame_duration=frame_duration)
         assert len(images), (label, name)
 
         if max_examples:
@@ -50,12 +53,15 @@ def make_images_for_class(label, images_dir, audio_dir, max_examples):
 @click.option('-i', '--images-dir', default=DEFAULT_IMAGES_DIR,
               show_default=True)
 @click.option('-m', '--max-examples-per-class', 'max_examples', type=int)
-def main(audio_dir, images_dir, max_examples):
+@click.option('-d', '--frame-duration', default=DEFAULT_FRAME_DURATION,
+              show_default=True)
+def main(audio_dir, images_dir, max_examples, frame_duration):
     if max_examples is not None and max_examples < 1:
         sys.exit('E: max_examples must be > 0')
 
     class_counts = {
-        cls: make_images_for_class(cls, images_dir, audio_dir, max_examples)
+        cls: make_images_for_class(
+            cls, images_dir, audio_dir, max_examples, frame_duration)
         for cls in os.listdir(audio_dir)
     }
 
