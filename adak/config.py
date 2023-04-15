ec07ac06b2abbb43ac49a4524bd685637085e49b
tim@brandywine <tim@brandywine>
2023-04-15 15:43:08 -0700

    Train on five second images that overlap by 50%

--- adak/config.py
+++ adak/config.py
@@ -7,8 +7,8 @@ class TrainConfig:
     n_fft = 1024
     hop_length = n_fft // 2
     sample_rate = 32000
-    frame_duration = 10.
-    frame_hop_length = 1 + int(frame_duration*sample_rate) // hop_length
+    frame_duration = 5.
+    frame_hop_length = (1 + int(frame_duration*sample_rate) // hop_length) // 2
     pad_remainder = True
     min_examples = 10
     max_examples = None
