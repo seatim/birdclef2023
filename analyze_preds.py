54e0bf94bac6fd2f87533803fc9851559378c650
tim@brandywine <tim@brandywine>
2023-04-25 08:21:27 -0700

    Add program to analyze predictions

---
+++ analyze_preds.py
@@ -0,0 +1,72 @@
+
+import sys
+
+from os.path import dirname, join
+
+import click
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from tabulate import tabulate
+
+
+def get_bc23_classes(path):
+    tmd = pd.read_csv(join(dirname(path), '..', 'train_metadata.csv'))
+    return set(tmd.primary_label)
+
+
+def show_dist(series, desc, show_hist):
+    print()
+    print(f'Statistics of {desc}')
+    print(series.describe())
+
+    if show_hist:
+        ax = series.hist()
+        ax.set_title(f'Histogram of {desc}')
+        plt.show()
+
+
+@click.command()
+@click.argument('path')
+@click.option('-s', '--show-hist', is_flag=True)
+@click.option('-p', '--threshold', type=float)
+def main(path, show_hist, threshold):
+
+    if (threshold is not None) and not (0 < threshold < 1):
+        sys.exit('E: threshold must be between 0 and 1.')
+
+    df = pd.read_csv(path, index_col=0)
+    classes = list(get_bc23_classes(path))
+    assert all(name in df.columns for name in classes), 'missing classes'
+
+    df = df.set_index('path')
+    assert sum(df.index.duplicated()) == 0, 'path column is not unique'
+
+    show_dist(df.sum(axis=1), 'sum of predictions over all classes', show_hist)
+    show_dist(df[classes].sum(axis=1), 'sum of predictions over bc23 classes',
+              show_hist)
+
+    if threshold:
+        all_classes = np.array(df.columns)
+        df['sum_bc23'] = df[classes].sum(axis=1)
+        lp = df[df['sum_bc23'] < threshold]
+        table = [[path, row['sum_bc23']] for path, row in lp.iterrows()]
+
+        print()
+        print('Examples with sum of bc23 class predictions below threshold:')
+        print(tabulate(table, headers=['path', 'sum_bc23']))
+
+        print()
+        print('Top five predictions for the first of these examples:')
+        for path, _ in table[:5]:
+            print(path)
+            row = df.loc[path]
+            top5 = list(all_classes[row[all_classes].argsort()[-5:]])
+            top5preds = list(row[top5])
+            print(tabulate([top5 + [f'{p:.4f}' for p in top5preds]]))
+            print()
+
+
+if __name__ == '__main__':
+    main()
