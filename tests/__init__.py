5b3228ef1ef4fe72ab98103ca8da7a10defea3b9
tim@brandywine <tim@brandywine>
2023-05-25 07:59:27 -0700

    Print captured stdout if exception occurs in test

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,10 +6,14 @@ import io
 def run_main(main, args):
     f = io.StringIO()
 
-    with contextlib.redirect_stdout(f):
-        try:
-            main(args)
-        except SystemExit:
-            pass
+    try:
+        with contextlib.redirect_stdout(f):
+            try:
+                main(args)
+            except SystemExit:
+                pass
+    except:
+        print(f.getvalue())
+        raise
 
     return f.getvalue()
