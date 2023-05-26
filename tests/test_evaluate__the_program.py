06c081ffcf321780b03ccada333cc0a0cc94bc97
tim@brandywine <tim@brandywine>
2023-05-25 17:00:50 -0700

    Add asserts

--- tests/test_evaluate__the_program.py
+++ tests/test_evaluate__the_program.py
@@ -25,20 +25,24 @@ class Test_evaluate(unittest.TestCase):
     def test1(self):
         args = ['-S', IMAGES_DIR, PRETRAINED_MODEL_PATH]
         output = run_main(evaluate, args)
+        self.assertIn('20 inferences', output)
 
     def test_ensemble(self):
         args = ['-S', IMAGES_DIR, PRETRAINED_MODEL_PATH, PRETRAINED_MODEL_PATH]
         output = run_main(evaluate, args)
+        self.assertIn('20 inferences', output)
 
     def test_ensemble_efficient(self):
         args = ['-S', IMAGES_DIR, PRETRAINED_MODEL_PATH, PRETRAINED_MODEL_PATH,
                 '-t', '0.5']
         output = run_main(evaluate, args)
+        self.assertIn('20 inferences', output)
 
     def test_save_preds(self):
         preds_path = join(self.preds_dir, 'preds.csv')
         args = ['-S', IMAGES_DIR, PRETRAINED_MODEL_PATH, '-s', preds_path]
         output = run_main(evaluate, args)
+        self.assertIn('20 inferences', output)
 
         self.assertEqual(os.listdir(self.preds_dir), ['preds.csv'])
         df = pd.read_csv(preds_path, index_col=0)
