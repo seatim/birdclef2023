82bdb36455516b1aed44dd019d80c02e7edc7433
tim@brandywine <tim@brandywine>
2023-05-30 09:16:25 -0700

    Add doc strings

--- adak/pretrain.py
+++ adak/pretrain.py
@@ -13,6 +13,16 @@ from fastcore.basics import store_attr
 
 
 def _get_model_config(arch, n_in=3):
+    """Return default configuration of `timm` model.
+
+    Args:
+        arch (str): architecture name
+        n_in (int): number of image channels
+
+    Returns:
+        dict
+
+    """
     model = timm.create_model(
         arch, pretrained=True, num_classes=0, in_chans=n_in)
     return model.default_cfg
@@ -23,6 +33,22 @@ def make_pretrain_learner(pre_learn, dls, metrics, normalize=True,
                           lr=learner_defaults.lr, cbs=None, path=None,
                           model_dir='models', wd=None, wd_bn_bias=False,
                           train_bn=True, moms=(0.95,0.85,0.95)):
+
+    """Create a `fastai.learner.Learner` using ``pre_learn`` as a template.
+    The body of ``pre_learn``'s model is retained and a new head is created
+    that matches the classes in ``dls``.
+
+    Args:
+        pre_learn (obj): a `fastai.learner.Learner` object
+        dls (obj): a `fastai.data.core.DataLoaders` object
+        metrics (list)
+        normalize (bool)
+        ...
+
+    Returns:
+        A `fastai.learner.Learner` object
+
+    """
     arch = pre_learn.arch
     pretrained = True
     assert pre_learn.pretrained == True, pre_learn.pretrained
