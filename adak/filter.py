612ca7cd9e2b44bb078bd3f0ffaa03be26a95f06
tim@brandywine <tim@brandywine>
2023-05-30 07:02:21 -0700

    Add doc strings

--- adak/filter.py
+++ adak/filter.py
@@ -8,6 +8,18 @@ import numpy as np
 
 
 def top_k_filter(preds, k, assert_input_is_normalized=False):
+    """Return modified copy of ``preds``.  For each prediction in ``preds``,
+    keep the K highest scores and set the rest to zero.  Then scale the
+    remaining scores so they sum to one.
+
+    Args:
+        preds (array): 2D NumPY array
+        k (int): the K in "K highest"
+
+    Returns:
+        2D NumPY array
+
+    """
 
     def top_k_filter(pred):
         if assert_input_is_normalized:
@@ -28,6 +40,18 @@ def top_k_filter(preds, k, assert_input_is_normalized=False):
 
 
 def fine_filter(preds, threshold, assert_input_is_normalized=False):
+    """Return modified copy of ``preds``.  For each prediction in ``preds``,
+    set scores that are lower than ``threshold`` to zero.  Then scale the
+    remaining scores so they sum to one.
+
+    Args:
+        preds (array): 2D NumPY array
+        threshold (float)
+
+    Returns:
+        2D NumPY array
+
+    """
     if not (0 < threshold < 1):
         raise ValueError('threshold must be between zero and one')
 
@@ -45,5 +69,17 @@ def fine_filter(preds, threshold, assert_input_is_normalized=False):
 
 
 def max_filter(preds, threshold):
+    """Return modified copy of ``preds``.  For each prediction in ``preds``,
+    if the highest score in the prediction vector is less than ``threshold``
+    set the vector to zero.
+
+    Args:
+        preds (array): 2D NumPY array
+        threshold (float)
+
+    Returns:
+        2D NumPY array
+
+    """
     return np.stack([(np.zeros(len(pred)) if max(pred) < threshold else pred)
                      for pred in np.array(preds)])
