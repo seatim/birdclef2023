464787d98efcd0e28bb641cc4b534bb2fd8f2dad
tim@brandywine <tim@brandywine>
2023-05-30 07:35:56 -0700

    Add get_y parameter to StratifiedSplitter

--- adak/glue.py
+++ adak/glue.py
@@ -8,13 +8,14 @@ from .evaluate import avg_precision  # part of this module's API now
 
 
 class StratifiedSplitter:
-    def __init__(self, test_size, random_state, print_stats=True):
+    def __init__(self, test_size, random_state, get_y=None, print_stats=True):
         self.test_size = test_size
         self.random_state = random_state
         self.print_stats = print_stats
+        self.get_y = get_y or parent_label
 
     def __call__(self, items):
-        labels = np.array([parent_label(item) for item in items])
+        labels = np.array([self.get_y(item) for item in items])
         train, valid = train_test_split(
             list(range(len(items))), test_size=self.test_size,
             random_state=self.random_state, stratify=labels, train_size=None,
