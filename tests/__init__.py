3cdbc93e4466aa8b9a6c486fe22a1e72fae90ee7
tim@brandywine <tim@brandywine>
2023-05-30 09:46:24 -0700

    Add unit test

--- tests/__init__.py
+++ tests/__init__.py
@@ -17,3 +17,16 @@ def run_main(main, args):
         raise
 
     return f.getvalue()
+
+
+def capture_stdout(func, *args, **kwargs):
+    f = io.StringIO()
+
+    try:
+        with contextlib.redirect_stdout(f):
+            func(*args, **kwargs)
+    except:
+        print(f.getvalue())
+        raise
+
+    return f.getvalue()
--- tests/test_sed.py
+++ tests/test_sed.py
@@ -4,9 +4,26 @@ import unittest
 import soundfile
 
 from adak.config import BaseConfig
-from adak.sed import sound_event_proba
+from adak.sed import SoundEventDetectionFilter, sound_event_proba
 
+from . import capture_stdout
 from .test_transform import TEST_AUDIO_PATH
+from .test_train_classifier import NSE_FILE_PATH
+
+
+class TestSoundEventDetectionFilter(unittest.TestCase):
+    def test1(self):
+        sed = SoundEventDetectionFilter(NSE_FILE_PATH, NSE_FILE_PATH)
+        self.assertIsNone(sed.threshold)
+
+        def set_threshold(thr):
+            sed.threshold = thr
+
+        output = capture_stdout(set_threshold, 0.199)
+        self.assertEqual(sed.threshold, 0.199)
+
+        output = capture_stdout(sed.examine)
+        self.assertIn('20 unique hashes', output)
 
 
 class Test_sound_event_proba(unittest.TestCase):
